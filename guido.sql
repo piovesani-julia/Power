9ad2a644ec4c3df0e77d0e1475a454bd745973a4
Ruh-Marcondes <marcondes.ruahma@gmail.com>
2019-09-04 18:58:27 -0300

    ??

--- guido.sql
+++ guido.sql
@@ -9,6 +9,7 @@ use power;
 /*
 ISBN
 descr - babulhos q contam um pouco da histório do livro 
+De estudo em vermelho até (....) - Ruh tem esses livros 
 */
 
 create table livros(
@@ -16,26 +17,26 @@ isbn int not null auto_increment,
 titulo varchar(35) not null,
 nautor varchar(100) not null,
 descr varchar(400),
+editora varchar(35),
 primary key (isbn)
 );
 
 -- parte chata insert into 
 
-insert into (titulo,nautor,descr)
-values (' A CUlpa é das Estrelas ','John Green','foda -se'),
-		('Harry Potter e o Prisioneiro de Azkarban',' J.K.Rowling','DFC'),
-        ('','',''),
-        ('','',''),
-        ('','',''),
-        ('','',''),
-        ('','',''),
-        ('','',''),
-        ('','',''),
-        ('','',''),
-        ('','',''),
-        ('','',''),
-        ('','',''),
-        ('','',''),
-        ('','',''),
-        ('','',''),
-        ('','',''),
+insert into (titulo,nautor,descr,editora)
+values  ('A CUlpa é das Estrelas ','John Green','foda -se',''),
+		('Harry Potter e o Prisioneiro de Azkarban',' J.K.Rowling','DFC','Rocco'),
+        ('Um Estudo em Vermelho','Artur Conon Doyle','','Zahar'),
+        ('Divirgente','Veronica Roth','','Rocco'),
+        ('Insurgente','Veronica Roth','','Rocco'),
+		('Convigente','Veronica Roth','','Rocco'),
+		('A Seleção','Kiera Cass','','Seguinte'),
+		('A Elite','Kiera Cass','','Seguinte'),
+        ('','','',''),
+        ('','','',''),
+        ('','','',''),
+        ('Marly & Eu ','Jonhon Grogan','Vc chora pra caralho lendo isso ','Ediouro'),
+		('Buracos Negros','Stephen Hawking','Ainda n li','Intrinca'), -- melhor editora 
+		('Os Relógios ','Agatha Cristie ','Um caso de Hercule Poirot','GloboLivros'),
+		('','','',''),
+        ('Clube Da Luta','Chuck Palahniuk','','LeYa'),
