a907742a5525afac366fd05481dee72d8309ae4f
Ruh-Marcondes <marcondes.ruahma@gmail.com>
2019-09-04 19:30:59 -0300

    toma no meu cu porrah

--- guido.sql
+++ guido.sql
@@ -14,7 +14,7 @@ De estudo em vermelho até (....) - Ruh tem esses livros
 
 create table livros(
 isbn int not null auto_increment,
-titulo varchar(35) not null,
+titulo varchar(100) not null,
 nautor varchar(100) not null,
 descr varchar(400),
 editora varchar(35),
@@ -23,7 +23,7 @@ primary key (isbn)
 
 -- parte chata insert into 
 
-insert into (titulo,nautor,descr,editora)
+insert into livros (titulo,nautor,descr,editora)
 values  ('A CUlpa é das Estrelas ','John Green','foda -se',''),
 		('Harry Potter e o Prisioneiro de Azkarban',' J.K.Rowling','DFC','Rocco'),
         ('Um Estudo em Vermelho','Artur Conon Doyle','','Zahar'),
@@ -32,11 +32,11 @@ values  ('A CUlpa é das Estrelas ','John Green','foda -se',''),
 		('Convigente','Veronica Roth','','Rocco'),
 		('A Seleção','Kiera Cass','','Seguinte'),
 		('A Elite','Kiera Cass','','Seguinte'),
-        ('','','',''),
-        ('','','',''),
-        ('','','',''),
+        ('A Escolha ','Kiera Cass','','Seguinte'),
+        ('A Herdeira','Kiera Cass','','Seguinte'),
         ('Marly & Eu ','Jonhon Grogan','Vc chora pra caralho lendo isso ','Ediouro'),
 		('Buracos Negros','Stephen Hawking','Ainda n li','Intrinca'), -- melhor editora 
 		('Os Relógios ','Agatha Cristie ','Um caso de Hercule Poirot','GloboLivros'),
-		('','','',''),
-        ('Clube Da Luta','Chuck Palahniuk','','LeYa'),
+		('Contos de Kolimá','Varlam Chalámov','nunca nem li','editora34'),
+        ('Clube Da Luta','Chuck Palahniuk','','LeYa');
+	
