558d6389dfef2e859eb6aa4ff2cba6c1a7683fe7
Ruh-Marcondes <marcondes.ruahma@gmail.com>
2019-09-06 19:20:06 -0300

    conexao

--- conexao.php
+++ conexao.php
@@ -2,7 +2,7 @@
     $username = 'root';
     $password = '';
     try{
-        $conn = new PDO('mysql:host=localhost;dbname=guido',
+        $conn = new PDO('mysql:host=localhost;dbname=power',
         $username,
         $password);      
         $conn ->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
