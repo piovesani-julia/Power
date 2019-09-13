d1f9460cb5f8fb1d424f0a6c9d9ba86ca0fe93d6
Ruh-Marcondes <marcondes.ruahma@gmail.com>
2019-09-13 00:14:39 -0300

    index

---
+++ cardes.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<!--- uma pag com todos os produtos: dividido e com fotos + buscador de produtos -->
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <!--Import Google Icon Font-->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <!-- add materialize-->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+  <link rel="stylesheet" href="materialize/css/custon.css">
+  <title>Power Livros</title>
+</head>
+
+<body>
+  <!-- PRESCISA -SE MENU-->
+
+  <div class="container">
+    <div class="row">
+      <div class="col s12 m7">
+      </div>
+      <div class="col s12 m7">
+        <div class="card">
+          <div class="card-image " style="width:50%">
+            <img src="Fotos/selecao.jpg">
+            <span class="card-title">SELECÃO</span>
+          </div>
+          <div class="card-content">
+            <p> A Seleção - Kiera Cass </p>
+          </div>
+          <div class="card-action">
+            <p>R$ 24,90</p>
+          </div>
+        </div>
+      </div> <!-- fecha a div class col -->
+      <!-- Carde horizontal -->
+      <div class="col s12 m7">
+        <div class="card horizontal">
+          <div class="card-image">
+            <img src="Fotos/selecao.jpg">
+          </div>
+          <div class="card-stacked">
+            <div class="card-content">
+              <p>A seleção - Kiera Cass</p>
+              <p>PROMOÇÃO</p>
+            </div>
+            <div class="card-action">
+              <p>R$ 25.90</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
