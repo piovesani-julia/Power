1c2983fa0770be5ea644f46fc7349bfc4886920f
Ruh-Marcondes <marcondes.ruahma@gmail.com>
2019-09-13 11:00:42 -0300

    Formulario pra salvar IMG

---
+++ formimg.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!--Import Google Icon Font-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!-- add materialize-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <link rel="stylesheet" href="materialize/css/custon.css">
+    <title>Power Livros</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+                <div class="co">
+                        <H1>
+                            Upload de Imagens 
+                        </H1>
+                        <h2>Mais pra freste junto com os livros </h2>
+                </div>
+        </div>
+    </div>
+</body>
+</html>
