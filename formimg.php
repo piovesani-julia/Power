5fcf952c41eb55e0e47590edf9a7e7b25b9b2492
Ruh-Marcondes <marcondes.ruahma@gmail.com>
2019-09-15 09:43:20 -0300

    add img e livro

--- formimg.php
+++ formimg.php
@@ -17,26 +17,28 @@
     <title>Power Livros</title>
 </head>
 <body>
-<h1>Cadastrar Imagem</h1>
-        <?php
-        if(isset($_SESSION['msg'])){
-            echo $_SESSION['msg'];
-            unset($_SESSION['msg']);
-        }
-        ?>
-        <form method="POST" action="proc_cad_img.php" enctype="multipart/form-data">
-            <label>Nome:</label>
-            <input type="text" name="nome" placeholder="Digitar o nome"><br><br>
-            
-            <label>Imagem</label>
-            <input type="file" name="imagem"><br><br>
-            
-            <input name="SendCadImg" type="submit" value="Cadastrar">
-        </form>
-        <br/>
-        <br/>
-        <hr>
-        <br/>
+    <h1>Cadastrar Imagem</h1>
+            <?php
+            if(isset($_SESSION['msg'])){
+                echo $_SESSION['msg'];
+                unset($_SESSION['msg']);
+            }
+            ?>
+            <form method="POST" action="proc_cad_img.php" enctype="multipart/form-data">
+                <label>Nome:</label>
+                <input type="text" name="nome" placeholder="Digitar o nome"><br><br>
+                
+                <label>Imagem</label>
+                <input type="file" name="imagem"><br><br>
+                
+                <input name="SendCadImg" type="submit" value="Cadastrar">
+            </form>
+            <br/>
+            <br/>
+            <hr>
+            <br/>
+            <br/>
+    <h1>Cadastrar Livros</h1>
         <a href="tabela.php"> @Tabela</a>
 </body>
 </html>
