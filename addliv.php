285c829aeebba6ab1f12acd1f944e2a081b7d18c
Ruh <47475949+Ruh-Marcondes@users.noreply.github.com>
2019-09-16 08:33:23 -0300

    add livro (php)

--- addliv.php
+++ addliv.php
@@ -1,31 +1,35 @@
 <?php
 require_once 'init.php';
 // pega os dados do formuário
-$name = isset($_POST['name']) ? $_POST['name'] : null;
-$email = isset($_POST['email']) ? $_POST['email'] : null;
-$gender = isset($_POST['gender']) ? $_POST['gender'] : null;
-$birthdate = isset($_POST['birthdate']) ? $_POST['birthdate'] : null;
- 
+$isbn = isset($_POST['isbn']) ? $_POST['isbn'] : null;
+$titulo = isset($_POST['titulo']) ? $_POST :null;
+$nome = isset($_POST['nome']) ? $_POST['email'] : null;
+$sobrenome = isset($_POST['sobrenome']) ? $_POST['gender'] : null;
+$editora = isset($_POST['editora']) ? $_POST['birthdate'] : null;
+$desc = isset($_POST['desc']) ? $_POST['desc'] : null;
+
 // validação (bem simples, só pra evitar dados vazios)
-if (empty($name) || empty($email) || empty($gender) || empty($birthdate))
+if (empty($titulo) || empty($nome) || empty($sobrenome)
 {
     echo "Volte e preencha todos os campos";
     exit;
 }
+
 // a data vem no formato dd/mm/YYYY
 // então precisamos converter para YYYY-mm-dd
 $isoDate = dateConvert($birthdate);
 // insere no banco
 $PDO = db_connect();
-$sql = "INSERT INTO users(name, email, gender, birthdate) VALUES(:name, :email, :gender, :birthdate)";
+$sql = "INSERT INTO livros(titulo, nome, sobrenome, editora, desc) VALUES(:titulo, :nome, :sobrenome, :gender, :editora, :desc)";
 $stmt = $PDO->prepare($sql);
-$stmt->bindParam(':name', $name);
-$stmt->bindParam(':email', $email);
-$stmt->bindParam(':gender', $gender);
-$stmt->bindParam(':birthdate', $isoDate);
+$stmt->bindParam(':titulo', $titulo);
+$stmt->bindParam(':nome', $nome);
+$stmt->bindParam(':sobrenome', $sobrenome);
+$stmt->bindParam(':editora', $editora);
+$stmt->bindParam(':desc', $desc);
 if ($stmt->execute())
 {
-    header('Location: index.php');
+    header('Location: tabela.php');
 }
 else
 {
