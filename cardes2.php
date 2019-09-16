c497c30b132e760d9a19cce41b69b3a2e8f03818
Ruh-Marcondes <marcondes.ruahma@gmail.com>
2019-09-16 05:35:43 -0300

    Teste do que fazer

--- cardes2.php
+++ cardes2.php
@@ -1,3 +1,6 @@
+<?php
+    include_once './init.php';
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -14,7 +17,17 @@
 <body>
     <!-- Ver se da pra fazer um for ichie pra criar cardes conforme os registros e utiliazar as img  -->
     <?php
-        
+    //arrumando erro 
+    error_reporting(0);
+    $conn->exec("set names utf8");
+    //consulta para mostrar os livros em ordem alfabetica 
+    // $sql = "select titulo,autor,sobrenome,editora from livros order by titulo,sobrenome asc;";
+    $result = $conn->query($sql);
+    $livros = $result->fetchAll(PDO::FETCH_ASSOC);
     ?>
+    <!-- html -->
+    <div class="container">
+
+    </div>
 </body>
 </html>
