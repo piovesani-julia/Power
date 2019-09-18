4d73480ff1fbdbb54c5067aecb42fdd61d4dd2b8
Ruh-Marcondes <marcondes.ruahma@gmail.com>
2019-09-17 21:56:39 -0300

    ADD img (HTML)

---
+++ form.php
@@ -0,0 +1,40 @@
+<?php
+  session_start();
+  include_once'./init.php';
+
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!--Import Google Icon Font-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!-- add materialize-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <link rel="stylesheet" href="materialize/css/custon.css">
+    <title>Power Livros</title>
+</head>
+<body>
+<h1>Cadastrar Imagem</h1>
+        <?php
+         $conn->exec("set names utf8");
+        if(isset($_SESSION['msg'])){
+            echo $_SESSION['msg'];
+            unset($_SESSION['msg']);
+        }
+        ?>
+        <form method="POST" action="proc_cad_img.php" enctype="multipart/form-data">
+            <label>Nome:</label>
+                <input type="text" name="nome" placeholder="Digitar o nome"><br><br>
+            
+            <label>Imagem</label>
+                <input type="file" name="imagem"><br><br>
+            
+            <input name="SendCadImg" type="submit" value="Cadastrar">
+        </form>
+        <br/><br/>
+</body>
+</html>
