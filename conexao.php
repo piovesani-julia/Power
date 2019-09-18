21a8f9499d4368b6ff5e6e982747451a8de4773d
Ruh-Marcondes <marcondes.ruahma@gmail.com>
2019-09-17 21:56:13 -0300

    Pro BD e IMG

---
+++ conexao.php
@@ -0,0 +1,12 @@
+  
+<?php
+    $username = 'root';
+    $password = '';
+    try{
+        $conn = new PDO('mysql:host=localhost;dbname=guido',
+        $username,
+        $password);      
+        $conn ->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }catch(PDOException $e){
+    echo 'aconteceu o erro: '. $e->getMessage();        
+    }
