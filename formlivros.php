5167d569797def58b473f54589286cc84e436721
Ruh <47475949+Ruh-Marcondes@users.noreply.github.com>
2019-09-22 11:15:38 -0300

    Cadastrar Livro

--- formlivros.php
+++ formlivros.php
@@ -19,21 +19,21 @@
        <br/>
        <div class="container">
             <div class="row">
-                <div class="col s1"></div>
+                <div class="col s1">
+                </div>
                 <div class="col s10">
                 <h1>Cadastrar Livros</h1>
         <form action="addliv.php" method = "POST">
-        <label>ISBN:</label>
+            <label>ISBN:</label>
                 <input type="text" nome="isbn" placeholder="Digite o ISBN do livro"><br/><br/>
-            <label>Titulo: </label>
+            <label>Titulo:</label>
                 <input type="text" nome="titulo" placeholder="Digite o título"><br/>
             <label>Autor:</label>
-                <input type="text" nome="nome" placeholder="Digite o nome do autor"><br/><br/>
+                <input type="text" nome="autor" placeholder="Digite o nome do autor"><br/><br/>
             <label>Sobrenome:</label>
                 <input type="text" nome="sobrenome" placeholder="Digite o sobrenome do autor"><br/><br/>
             <label>Editora:</label>
                 <input type="text" nome="editora" placeholder="Digite a editora"><br/><br/>
-
                 <input name="SendCadImg" type="submit" value="Cadastrar">
         </form>
                 </div>
