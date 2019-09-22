20b7a71a80fe4b26801c682405f13a0b141a3584
Ruh <47475949+Ruh-Marcondes@users.noreply.github.com>
2019-09-22 11:15:18 -0300

    Vitrine

--- cardes.php
+++ cardes.php
@@ -16,12 +16,36 @@
 
 <body>
   <!-- PRESCISA -SE MENU-->
+  <div class="container">
+            <div class="row">
+                <div class="col s1"></div>
+                <div class="col s5">
+                <h1>Livros</h1><br/>
+                <a href="formlivros.php">@Add registro</a><br/>
+                    <a href="cardes.php" >@Vitrine</a><br/>
+                </div>
+                <div class="col s5">
+                    <a href="addimg.php" >@Add Imagens</a><br/>
+                </div>
+                <div class="col s1"></div>
+            </div>
+            <div class="row">
+              <div class="col s1"></div>
+              <div class="col s10"><h6>Tudo depende  do tipo de lente que você usa pra ver as coisas ~
+                <br/>~ <i>O Mundo de Sofia</i>
+              </h6>
+            </div>
+            <div class="col s1"></div>
+          </div>
+            
+        </div>
+     <hr> <br />
   <div class="container"> 
       <div class="row"> 
         <div class="col s1 ">
 
         </div>
-        <div class="col s5">    
+        <div class="col s5">  
           <div class="card horizontal">
             <div class="card-image">
               <img src="Fotos/selecao.jpg">
@@ -58,7 +82,49 @@
         <div class="col s1">
 
         </div>
-      </div >
+      </div ><!-- Fecha a row -->
+      <div class="row"> 
+        <div class="col s1 ">
+
+        </div>
+        <div class="col s5">    
+          <div class="card horizontal">
+            <div class="card-image">
+              <img src="Fotos/alasca.jpg">
+            </div>
+            <div class="card-stacked">
+              <div class="card-content">
+                <p>Quem é você Alasca - Jonh Green</p>
+                <br/><br/> <br/><br/><br/>
+                <a href="#"></a>
+              </div>
+              <div class="card-action">
+                <p>R$ 35.00</p>
+              </div>
+            </div>
+          </div>
+       </div> <!-- fecha col s5 -->
+       <div class="col s5">    
+          <div class="card horizontal">
+            <div class="card-image">
+              <img src="Fotos/relogios.jpg">
+            </div>
+            <div class="card-stacked">
+              <div class="card-content">
+                <p>Os Relógios - Agatha Cristie</p>
+                <br/><br/> <br/><br/><br/>
+                <a href="#"></a>
+              </div>
+              <div class="card-action">
+                <p>R$ 35.00</p>
+              </div>
+            </div>
+          </div>
+       </div> <!-- fecha col s5 -->
+        <div class="col s1">
+
+        </div>
+      </div ><!-- Fecha a Row -->
   </div>
   
 </body>
