ea9a6a79f61718bc3078d4cd09f51db41a74fdfb
Ruh <47475949+Ruh-Marcondes@users.noreply.github.com>
2019-09-22 10:25:57 -0300

    Tabela

--- tabela.php
+++ tabela.php
@@ -22,8 +22,8 @@ include_once './conexao.php';
         <div class="row">
             <div class="col s1"></div>
             <div class="col s10">
-                <div class="card-panel color blue text-cyan  text-lighten-5 ">
-                    <h2> Tabela de livros </h2>
+                <div class="card-panel black ">
+                    <h2><span class="grey-text text-lighthen-5">Tabela de livros <#></span></h2>
                 </div>
             </div>
             <div class="col s1"></div>
@@ -31,16 +31,18 @@ include_once './conexao.php';
         <br /> <hr> <br />
         <div class="container">
             <div class="row">
-                <div class="col s2"></div>
+                <div class="col s1"></div>
                 <div class="col s5">
                 <a href="formlivros.php">@Add registro</a><br/>
                     <a href="cardes.php" >@Vitrine</a><br/>
                 </div>
                 <div class="col s5">
-                    <a href="addimg.php" >@Add Imagens</a>
+                    <a href="addimg.php" >@Add Imagens</a><br/>
                 </div>
-                <div class="col s2"></div>
+                <div class="col s1"></div>
             </div>
+            <div class="row"><div class="col s1"></div><div class="col s10"><h6>Tudo depende  do tipo de lente que você usa pra ver as coisas ~<br/>~ <i>O Mundo de Sofia</i></h6></div><div class="col s1"></div></div>
+            
         </div>
      <hr> <br />
     </div>
@@ -50,7 +52,7 @@ include_once './conexao.php';
     error_reporting(0);
     $conn->exec("set names utf8");
     //consulta para mostrar os livros em ordem alfabetica 
-    $sql = "select titulo,autor,sobrenome,editora,isbn from livros order by titulo,sobrenome asc;";
+    $sql = "select titulo,autor,sobrenome,isbn,editora,id from livros order by titulo,sobrenome asc;";
     $result = $conn->query($sql);
     $livros = $result->fetchAll(PDO::FETCH_ASSOC);
     ?>
@@ -59,14 +61,16 @@ include_once './conexao.php';
         <div class="row">
             <div class="col s2"></div>
             <div class="col s8">
-            <table class="higlight">
+            <table class="centered">
                 <thead>
                     <tr>
                         <th> Titulo </th>
                         <th> Autor </th>
                         <th> Sobrenome </th>
-                        <th> Editora</th>
-                        <th>ISBN</th>
+                        <th> ISBN</th>
+                        <th>Editora</th>
+                        <th>ID de cadastro</th>
+                        <th>Ação</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -77,8 +81,16 @@ include_once './conexao.php';
                             <td><?= $livro[titulo] ?></td>
                             <td><?= $livro[autor] ?></td>
                             <td><?= $livro[sobrenome] ?></td>
-                            <td><?= $livro[editora] ?></td>
-                            <td><?=$livro[isbn]?></td>
+                            <td><?= $livro[isbn] ?></td>
+                            <td><?=$livro[editora]?></td>
+                            <td><?=$livro[id]?></td>
+                            <td>
+                            <a href="editliv.php?id=<?php echo $user['id'] ?>">Editar</a>
+                                <a href="dellivro.php?id=<?php echo $user['id'] ?>" 
+                                   onclick="return confirm('Tem certeza de que deseja remover?');">
+                                    Remover
+                                </a>
+                            </td>
                         </tr>
                     <?php
                     }
