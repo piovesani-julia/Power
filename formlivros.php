7b9a4954f29ca1378595320859e8b9265a8b0538
Ruh <47475949+Ruh-Marcondes@users.noreply.github.com>
2019-09-22 15:50:02 -0300

    Formulario dos livros

--- formlivros.php
+++ formlivros.php
@@ -14,31 +14,30 @@
 </head>
 
 <body>
-<hr>
-        <br/>
-       <br/>
-       <div class="container">
-            <div class="row">
-                <div class="col s1">
-                </div>
-                <div class="col s10">
-                <h1>Cadastrar Livros</h1>
-        <form action="addliv.php" method = "POST">
-            <label>ISBN:</label>
-                <input type="text" nome="isbn" placeholder="Digite o ISBN do livro"><br/><br/>
-            <label>Titulo:</label>
-                <input type="text" nome="titulo" placeholder="Digite o título"><br/>
-            <label>Autor:</label>
-                <input type="text" nome="autor" placeholder="Digite o nome do autor"><br/><br/>
-            <label>Sobrenome:</label>
-                <input type="text" nome="sobrenome" placeholder="Digite o sobrenome do autor"><br/><br/>
-            <label>Editora:</label>
-                <input type="text" nome="editora" placeholder="Digite a editora"><br/><br/>
-                <input name="SendCadImg" type="submit" value="Cadastrar">
-        </form>
-                </div>
-                <div class="col s1"></div>
+    <br/>
+    <br/>
+    <div class="container">
+        <div class="row">
+            <div class="col s1 ">
             </div>
+            <div class="col s10">
+            <h1>Cadastrar Livros</h1>
+            <form action="addliv.php" method = "POST">
+                <label>ISBN:</label>
+                    <input type="text" nome="isbn" placeholder="Digite o ISBN do livro"><br/><br/>
+                <label>Titulo:</label>
+                    <input type="text" nome="titulo" placeholder="Digite o título"><br/>
+                <label>Autor:</label>
+                    <input type="text" nome="autor" placeholder="Digite o nome do autor"><br/><br/>
+                <label>Sobrenome:</label>
+                    <input type="text" nome="sobrenome" placeholder="Digite o sobrenome do autor"><br/><br/>
+                <label>Editora:</label>
+                    <input type="text" nome="editora" placeholder="Digite a editora"><br/><br/>
+                    <input name="SendCadImg" type="submit" value="Cadastrar">
+            </form>
+            </div>
+            <div class="col s1"></div>
         </div>
+    </div>
 </body>
 </html>
