8a22a2e7f4f24fd7a383bc5af87783105aa0a503
Ruh <47475949+Ruh-Marcondes@users.noreply.github.com>
2019-09-24 21:39:31 -0300

    Base de todos os arquivos "html"

--- base.php
+++ base.php
@@ -15,5 +15,30 @@
 
 <body>
 
+<footer class="page-footer black">
+          <div class="container">
+            <div class="row">
+              <div class="col l6 s12">
+                <h5 class="blue-text">Livraria Power </h5>
+                <p class="blue-text text-darken-4"><b>Livriria criada par fins educativos</b></p>
+              </div>
+              <div class="col l4 offset-l2 s12">
+                <h5 class="white-text"></h5>
+                <ul>
+                  <li>Ruahma</li>
+                  <li>Julia</li>
+                  <li>Lúcia</li>
+                  <li>Isadora</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="footer-copyright">
+            <div class="container">
+            © 2019 Trabalho Escolar 
+            <a class="grey-text text-lighten-4 right" href="b/indexb.php">More Links</a>
+            </div>
+          </div>
+        </footer>
 </body>
 </html>
