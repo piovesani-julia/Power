ab06cb619d45d4d244bbee43d6d8085859490957
Ruh <47475949+Ruh-Marcondes@users.noreply.github.com>
2019-09-24 21:41:13 -0300

    Edit PHP

--- edit.php
+++ edit.php
@@ -1,29 +1,26 @@
 <?php
-require_once 'init.php';
+require_once 'conexao.php';
 // resgata os valores do formulário
-$name = isset($_POST['name']) ? $_POST['name'] : null;
-$email = isset($_POST['email']) ? $_POST['email'] : null;
-$gender = isset($_POST['gender']) ? $_POST['gender'] : null;
-$birthdate = isset($_POST['birthdate']) ? $_POST['birthdate'] : null;
-$id = isset($_POST['id']) ? $_POST['id'] : null;
-// validação (bem simples, mais uma vez)
-if (empty($name) || empty($email) || empty($gender) || empty($birthdate))
-{
-    echo "Volte e preencha todos os campos";
-    exit;
-}
+$isbn = isset($_POST['isbn'])? $_POST['isbn'] : null;
+$titulo = isset($_POST['titulo']) ? $_POST['titulo'] : null;
+$autor = isset($_POST['autor']) ? $_POST['autor'] : null;
+$sobrenome = isset($_POST['sobrenome']) ? $_POST['sobrenome'] : null;
+$editora = isset($_POST['editora']) ? $_POST['editora'] : null;
+$preco = isset($_POST['preco']) ? $_POST['preco'] : null;
 // a data vem no formato dd/mm/YYYY
 // então precisamos converter para YYYY-mm-dd
 $isoDate = dateConvert($birthdate);
 // atualiza o banco
 $PDO = db_connect();
-$sql = "UPDATE users SET name = :name, email = :email,"
-        . " gender = :gender, birthdate = :birthdate WHERE id = :id";
+$sql = "UPDATE guido. livros  SET isbn = :isbn , titulo = :titulo, autor = :autor, sobrenome = :sobrenome, preco = :preco WHERE (id = :id);";
 $stmt = $PDO->prepare($sql);
-$stmt->bindParam(':name', $name);
-$stmt->bindParam(':email', $email);
-$stmt->bindParam(':gender', $gender);
-$stmt->bindParam(':birthdate', $isoDate);
+$stmt = $PDO->prepare($sql);
+$stmt->bindParam(':isbn',$isbn);
+$stmt->bindParam(':titulo',$titulo);
+$stmt->bindParam(':autor', $autor);
+$stmt->bindParam(':sobrenome', $sobrenome);
+$stmt->bindParam(':editora', $editora);
+$stmt->bindParam(':preco', $preco);
 $stmt->bindParam(':id', $id, PDO::PARAM_INT);
 if ($stmt->execute())
 {
