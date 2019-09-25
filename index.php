90b292bf2f593c1d597d123949d6578063889952
Ruh <47475949+Ruh-Marcondes@users.noreply.github.com>
2019-09-24 21:40:12 -0300

    Tabela

--- index.php
+++ index.php
@@ -1,19 +1,11 @@
 <?php
-require_once 'conexao/conexao.php';
-// abre a conexão
-$PDO = db_connect();
-
-error_reporting(0);
-$conn->exec("set names utf8");
-$sql = "SELECT isnb, titulo, autor,  sobrenome, id, editora, FROM livros ORDER BY name ASC";
-$result =$conn->query($sql);
-$livros = $result->fetchAll(PDO::FETCH_ASSOC)
+require_once 'conexao.php';
 ?>
 <!doctype html>
 <html>
 
 <head>
-    <meta charset="UTF-8">
+<meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <!--Import Google Icon Font-->
@@ -22,13 +14,25 @@ $livros = $result->fetchAll(PDO::FETCH_ASSOC)
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
     <link rel="stylesheet" href="materialize/css/custon.css">
     <title>Power Livros</title>
+    <style>
+        h4{
+            color:#4169e1;
+        }
+        h2
+        {
+            color:#1e90ff;
+        }
+    </style>
+</head>
 </head>
 
 <body>
     <?php
     error_reporting(0);
+    //aceitar acentuação 
     $conn->exec("set names utf8");
-    $sql = "Select isbn, titulo, autor, sobrenome, editora, id, preco from livros order by titulo, sobrenome ASC";
+    $sql = "Select isbn, titulo, autor, sobrenome, editora, id, preco" 
+    ."from livros order by titulo, sobrenome ASC";
     $result = $conn->query($sql);
     $livros = $result->fetchAll(PDO::FETCH_ASSOC);
     ?>
@@ -46,20 +50,17 @@ $livros = $result->fetchAll(PDO::FETCH_ASSOC)
             </div>
         </div>
         <div class="row">
-            <div class="col s2">
-                <!-- Espaço -->
-            </div>
+            <div class="col s2"><!-- Espaço --></div>
             <div class="col s5">
-                <!menu>
+                <!--menu-->
                 <ul>
-                <li>
-                <a href="form-add.php"> @add Livro</a>
-                </li>
+                    <li>
+                    <a href="form-add.php">@add Livro</a>
+                    </li>
                 </ul>
-                <hr>
             </div>
-            <div class="col s5">
-                <!menu>
+            <div class="col s5"><!--menu-->
+                <a href="#">@Promoção</a>
             </div>
             <div class="col s2">
                 <!-- Frase --> 
@@ -69,10 +70,8 @@ $livros = $result->fetchAll(PDO::FETCH_ASSOC)
     <div class="container">
         <div class="row">
         <div class="col s2"><!-- Espaço--></div>
-            <div class="col s10">
-                <!-- Tabela -->
-                <br/><hr><br/>
-                <table class=" ">
+            <div class="col s10"><!-- Tabela --><br/>
+                <table class="centered striped">
                     <thead>
                         <tr>
                             <th>ISBN</th>
@@ -87,7 +86,7 @@ $livros = $result->fetchAll(PDO::FETCH_ASSOC)
                     </thead>
                     <tbody>
                         <?php
-                        foreach ($alunos as $livro) {
+                        foreach ($livros as $livro) {
                             ?>
                             <tr>
                                 <td><?= $livro[isbn] ?></td>
@@ -98,11 +97,8 @@ $livros = $result->fetchAll(PDO::FETCH_ASSOC)
                                 <td><?= $livro[id] ?></td>
                                 <td><?= $livro[preco] ?></td>
                                 <td>
-                                <a href="form-edit.php?id=<?php echo $livro['id'] ?>">Editar</a>
-                                <a href="delete.php?id=<?php echo $livro['id'] ?>" 
-                                   onclick="return confirm('Tem certeza de que deseja remover?');">
-                                    Remover
-                                </a>
+                                    <a href="form-edit.php?id=<?php echo $livro['id'] ?>">Editar</a>
+                                    <a href="delete.php?id=<?php echo $livro['id'] ?>">Remover</a>
                                 </td>
                             </tr>
                         <?php
@@ -116,6 +112,31 @@ $livros = $result->fetchAll(PDO::FETCH_ASSOC)
             </div>
         </div><!-- Fecha div row-->
     </div>
+    <footer class="page-footer black">
+          <div class="container">
+            <div class="row">
+              <div class="col l6 s12">
+                <h5 class="blue-text">Livraria Power </h5>
+                <p class="blue-text text-darken-4"><b>Livriria criada par fins educativos</b></p>
+              </div>
+              <div class="col l4 offset-l2 s12">
+                <h5 class="white-text"></h5>
+                <ul>
+                  <li>Ruahma</li>
+                  <li>Julia</li>
+                  <li>Lúcia</li>
+                  <li>Isadora</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="footer-copyright">
+            <div class="container">
+            © 2019 Trabalho Escolar 
+            <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+            </div>
+          </div>
+        </footer>
 </body>
 
 </html>
