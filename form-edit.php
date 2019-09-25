c88a7445e2718c8e0e347b2536c5d3d0c1b9ec24
Ruh <47475949+Ruh-Marcondes@users.noreply.github.com>
2019-09-24 21:41:32 -0300

    Form - Edit "html"

--- form-edit.php
+++ form-edit.php
@@ -56,5 +56,30 @@ if (!is_array($user)) {
             <input type="hidden" name="id" value="<?php echo $id ?>">
             <input type="submit" value="Alterar">
         </form>
+        <footer class="page-footer black">
+          <div class="container">
+            <div class="row">
+              <div class="col l6 s12">
+                <h5 class="blue-text">Livraria Power </h5>
+                <p class="blue-text text-darken-4"><b>Livriria criada par fins educativos</b></p>
+              </div>
+              <div class="col l4 offset-l2 s12">
+                <h5 class="white-text"></h5>
+                <ul>
+                  <li>Ruahma</li>
+                  <li>Julia</li>
+                  <li>Lúcia</li>
+                  <li>Isadora</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="footer-copyright">
+            <div class="container">
+            © 2019 Trabalho Escolar 
+            <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+            </div>
+          </div>
+        </footer>
     </body>
 </html>
