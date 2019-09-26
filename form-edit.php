7fce5dbdde67e4af0334672f2124fa9327a05ece
Ruh <47475949+Ruh-Marcondes@users.noreply.github.com>
2019-09-26 13:01:09 -0300

    Edit Livro

--- form-edit.php
+++ form-edit.php
@@ -1,85 +1,98 @@
 <?php
-require 'init.php';
+include_once 'conexao.php';
+
 // pega o ID da URL
 $id = isset($_GET['id']) ? (int) $_GET['id'] : null;
 // valida o ID
 if (empty($id)) {
-    echo "ID para alteração não definido";
-    exit;
+  echo "ID para alteração não definido";
+  exit;
 }
 // busca os dados do usuário a ser editado
 $PDO = db_connect();
-$sql = "SELECT name, email, gender, birthdate FROM users WHERE id = :id";
+$sql = "SELECT isbn,titulo,autor,sobrenome,editora,preco FROM livros WHERE id = :id";
 $stmt = $PDO->prepare($sql);
 $stmt->bindParam(':id', $id, PDO::PARAM_INT);
 $stmt->execute();
-$user = $stmt->fetch(PDO::FETCH_ASSOC);
+$livro = $stmt->fetch(PDO::FETCH_ASSOC);
 // se o método fetch() não retornar um array, significa que o ID não corresponde 
 // a um usuário válido
-if (!is_array($user)) {
-    echo "Nenhum usuário encontrado";
-    exit;
+if (!is_array($livro)) {
+  echo "Nenhum Livro Encontrado";
+  exit;
 }
 ?>
 <!doctype html>
 <html>
-    <head>
-        <meta charset="utf-8">
-        <title>Edição de Usuário</title>
-    </head>
-    <body>
-        <h1>Sistema de Cadastro</h1>
-        <h2>Edição de Usuário</h2>
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <!--Import Google Icon Font-->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <!-- add materialize-->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+  <link rel="stylesheet" href="materialize/css/custon.css">
+  <title>Power Livros</title>
+</head>
+
+<body>
+  <div class="container">
+    <div class="row">
+      <div class="col s1 ">
+      </div>
+      <div class="col s10">
+        <div class="card-panel black blue-text text-darken-2">
+          <h2>Cadastrar Livros</h2>
+          <h4>Edição de Livros</h4>
+        </div>
         <form action="edit.php" method="post">
-            <label for="name">Nome: </label>
-            <br>
-            <input type="text" name="name" id="name" value="<?php echo $user['name'] ?>">
-            <br><br>
-            <label for="email">Email: </label>
-            <br>
-            <input type="text" name="email" id="email" value="<?php echo $user['email'] ?>">
-            <br><br>
-            Gênero:
-            <br>
-            <input type="radio" name="gender" id="gender_m" value="m" <?php if ($user['gender'] == 'm'): ?> 
-                   checked="checked" <?php endif; ?>>
-            <label for="gener_m">Masculino </label>
-            <input type="radio" name="gender" id="gender_f" value="f" <?php if ($user['gender'] == 'f'): ?> 
-                   checked="checked" <?php endif; ?>>
-            <label for="gener_f">Feminino </label>
-            <br><br>
-            <label for="birthdate">Data de Nascimento: </label>
-            <br>
-            <input type="text" name="birthdate" id="birthdate" placeholder="dd/mm/YYYY" 
-                   value="<?php echo dateConvert($user['birthdate']) ?>">
-            <br><br>
-            <input type="hidden" name="id" value="<?php echo $id ?>">
-            <input type="submit" value="Alterar">
+          <label>ISBN:</label>
+          <input type="text" nome="isbn" placeholder="Digite o ISBN do livro"><br /><br />
+          <label>Titulo:</label>
+          <input type="text" nome="titulo" placeholder="Digite o título"><br />
+          <label>Autor:</label>
+          <input type="text" nome="autor" placeholder="Digite o nome do autor"><br /><br />
+          <label>Sobrenome:</label>
+          <input type="text" nome="sobrenome" placeholder="Digite o sobrenome do autor"><br /><br />
+          <label>Editora:</label>
+          <input type="text" nome="editora" placeholder="Digite a editora">
+          <label>Preço:</label>
+          <input type="text" nome="preco" placeholder="Digite o Preço"><br /><br />
+          <input type="submit" value="Alterar">
         </form>
-        <footer class="page-footer black">
-          <div class="container">
-            <div class="row">
-              <div class="col l6 s12">
-                <h5 class="blue-text">Livraria Power </h5>
-                <p class="blue-text text-darken-4"><b>Livriria criada par fins educativos</b></p>
-              </div>
-              <div class="col l4 offset-l2 s12">
-                <h5 class="white-text"></h5>
-                <ul>
-                  <li>Ruahma</li>
-                  <li>Julia</li>
-                  <li>Lúcia</li>
-                  <li>Isadora</li>
-                </ul>
-              </div>
-            </div>
-          </div>
-          <div class="footer-copyright">
-            <div class="container">
-            © 2019 Trabalho Escolar 
-            <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
-            </div>
-          </div>
-        </footer>
-    </body>
+      </div>
+      <div class="col s1 ">
+      </div>
+    </div>
+  </div>
+  <footer class="page-footer black">
+    <div class="container">
+      <div class="row">
+        <div class="col l6 s12">
+          <h5 class="blue-text">Livraria Power </h5>
+          <p class="blue-text text-darken-4"><b>Livriria criada par fins educativos</b></p>
+        </div>
+        <div class="col l4 offset-l2 s12">
+          <h5 class="white-text"></h5>
+          <ul>
+            <li>Ruahma</li>
+            <li>Julia</li>
+            <li>Lúcia</li>
+            <li>Isadora</li>
+            <li></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="footer-copyright">
+      <div class="container">
+        © 2019 Trabalho Escolar
+        <a class="grey-text text-lighten-4 right" href="b/indexb.php">More Links</a>
+      </div>
+    </div>
+  </footer>
+</body>
+
 </html>
