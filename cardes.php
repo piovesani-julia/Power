fe826266b56885da7d6a07e435773b2d121b39e9
Ruh <47475949+Ruh-Marcondes@users.noreply.github.com>
2019-09-26 13:02:44 -0300

    INDETERMINADO

---
+++ cardes.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!--Import Google Icon Font-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!-- add materialize-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <link rel="stylesheet" href="materialize/css/custon.css">
+    <title>Power Livros</title>
+
+</head>
+
+<body>
+<div class="container">
+    <div class="row">
+        <div class="col s1"></div>
+        <div class="col s5">
+        <h2 class="header"></h2>
+    <div class="card horizontal">
+      <div class="card-image">
+        <img src="Fotos/alasca.jpg">
+      </div>
+      <div class="card-stacked">
+        <div class="card-content">
+        <br/>
+        <a href="#"><i class="small material-icons">add_shopping_cart</i></a>
+        <br/>
+        <br/><BR/><BR/>
+        <p>Quem é você Alasca?</p>
+        </div>
+        <div class="card-action">
+         <p>R$ 35,00</p>
+        </div>
+      </div>
+    </div>
+        </div>
+
+        <div class="col s5">
+        <h2 class="header"></h2>
+    <div class="card horizontal">
+      <div class="card-image">
+        <img src="Fotos/selecao.jpg">
+      </div>
+      <div class="card-stacked">
+        <div class="card-content">
+        <br/>
+        <a href="#"><i class="small material-icons">add_shopping_cart</i></a>
+        <br/>
+        <br/><BR/><BR/>
+        <p>Quem é você Alasca?</p>
+        </div>
+        <div class="card-action">
+         <p>R$ 24,90</p>
+        </div>
+      </div>
+    </div>
+        </div>
+        <div class="col s1"></div>
+    </div>
+</div>
+<div class="container">
+    <div class="row">
+        <div class="col s1"></div>
+        <div class="col s5">
+        <h2 class="header"></h2>
+    <div class="card horizontal">
+      <div class="card-image">
+        <img src="Fotos/alasca.jpg">
+      </div>
+      <div class="card-stacked">
+        <div class="card-content">
+        <br/>
+        <a href="#"><i class="small material-icons">add_shopping_cart</i></a>
+        <br/>
+        <br/><BR/><BR/>
+        <p>Quem é você Alasca?</p>
+        </div>
+        <div class="card-action">
+         <p>R$ 35,00</p>
+        </div>
+      </div>
+    </div>
+        </div>
+
+        <div class="col s5">
+        <h2 class="header"></h2>
+    <div class="card horizontal">
+      <div class="card-image">
+        <img src="Fotos/selecao.jpg">
+      </div>
+      <div class="card-stacked">
+        <div class="card-content">
+        <br/>
+        <a href="#"><i class="small material-icons">add_shopping_cart</i></a>
+        <br/>
+        <br/><BR/><BR/>
+        <p>Quem é você Alasca?</p>
+        </div>
+        <div class="card-action">
+         <p>R$ 35,00</p>
+        </div>
+      </div>
+    </div>
+        </div>
+        <div class="col s1"></div>
+    </div>
+</div>
+
+
+<footer class="page-footer black">
+          <div class="container">
+            <div class="row">
+              <div class="col l6 s12">
+                <h5 class="blue-text">Livraria Power </h5>
+                <p class="blue-text text-darken-4"><b>Livriria criada par fins educativos</b></p>
+              </div>
+              <div class="col l4 offset-l2 s12">
+                <h5 class="white-text"></h5>
+                <ul>
+                  <li>Ruahma</li>
+                  <li>Julia</li>
+                  <li>Lúcia</li>
+                  <li>Isadora</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="footer-copyright">
+            <div class="container">
+            © 2019 Trabalho Escolar 
+            <a class="grey-text text-lighten-4 right" href="b/indexb.php">More Links</a>
+            </div>
+          </div>
+        </footer>
+</body>
+</html>
