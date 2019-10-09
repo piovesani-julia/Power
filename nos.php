e2d633cbd0e3c176c4061e51a181c7f004d92160
Ruh-Marcondes <marcondes.ruahma@gmail.com>
2019-10-08 22:02:00 -0300

    About us

--- nos.php
+++ nos.php
@@ -23,31 +23,5 @@
 
 
 <!-- Rodapé -->
-<footer class="page-footer black">
-          <div class="container">
-            <div class="row">
-              <div class="col l6 s12">
-                <h5 class="blue-text">Livraria Power </h5>
-                <p class="blue-text text-darken-4"><b>Livriria criada par fins educativos</b></p>
-              </div>
-              <div class="col l4 offset-l2 s12">
-                <a href="nos.php"><h5 class="white-text"> Nós</h5></a>
-                <ul>
-                  <li>Ruahma</li>
-                  <li>Julia</li>
-                  <li>Lúcia</li>
-                  <li>Isadora</li>
-                  <li>Luigi</li>
-                </ul>
-              </div>
-            </div>
-          </div>
-          <div class="footer-copyright">
-            <div class="container">
-                © 2019 Trabalho Escolar 
-            <a class="grey-text text-lighten-4 right" href="b/indexb.php">More Link</a>
-            </div>
-          </div>
-        </footer>
 </body>
 </html>
